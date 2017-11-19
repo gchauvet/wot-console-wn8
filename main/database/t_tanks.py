b741a97ac3a67d018bfbfe5485cbdf2042a70c3f
IDDT <IDDT@users.noreply.github.com>
2017-11-19 22:25:34 +0700

    Optimized WN8 calculation. Removed old code.
    5x speed increase.

--- main/database/t_tanks.py
+++ main/database/t_tanks.py
@@ -28,39 +28,7 @@ def get_percentiles_data(tank_ids):
     return columns, data
 
 
-def get_wn8_data(tankopedia, tank_tier='all', tank_type='all'):
-    #Get dictionary of numpy arrays for WN8 calculation filtered by tier and type.
-
-    #Get tank ids.
-    tank_tiers = [tank_tier] if tank_tier != 'all' else list(range(1, 11))
-    tank_types = [tank_type] if tank_type != 'all' else ['lightTank', 'mediumTank', 'heavyTank', 'AT-SPG', 'SPG']
-    tank_ids = [key for key, val in tankopedia.items() if val['tier'] in tank_tiers and val['type'] in tank_types]
-
-    #Query into pandas dataframe.
-    columns_str = ', '.join(['tank_id', 'damage_dealt', 'spotted', 'frags', 'dropped_capture_points', 'wins', 'battles'])
-    tank_ids_str = ', '.join(tank_ids)
-    df = pd.read_sql(f'SELECT {columns_str} FROM tanks WHERE tank_id IN ({tank_ids_str});', conn)
-
-    #Calculations.
-    df['damage_dealt']           = df['damage_dealt']           / df['battles']
-    df['spotted']                = df['spotted']                / df['battles']
-    df['frags']                  = df['frags']                  / df['battles']
-    df['dropped_capture_points'] = df['dropped_capture_points'] / df['battles']
-    df['wins']                   = df['wins']                   / df['battles'] * 100
-
-    #Return as numpy arrays.
-    return({
-        'tank_id':                df['tank_id'].values,
-        'battles':                df['battles'].values,
-        'damage_dealt':           df['damage_dealt'].values,
-        'dropped_capture_points': df['dropped_capture_points'].values,
-        'frags':                  df['frags'].values,
-        'spotted':                df['spotted'].values,
-        'wins':                   df['wins'].values
-    })
-
-
-def get_dataframe(tank_ids, columns, min_battles=0):
+def get_dataframe(tank_ids, columns, min_battles=1):
 
     tank_ids_str = ', '.join([str(x) for x in tank_ids])
     columns_str = ', '.join(columns)
